307a71d24fbab7d4fb7bd0f4c1d27fb8e8849ff8
Tharindu Dilshan <jdtdjayasingha@gmail.com>
2024-04-24 22:24:50 +0530

    Created the Show Treatment Details Screen & Show Treatment data in Grid View

---
+++ Medicare Medical Center/ShowTreatmentDetails.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Medicare_Medical_Center
+{
+    partial class ShowTreatmentDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            treatmentGW = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)treatmentGW).BeginInit();
+            SuspendLayout();
+            // 
+            // treatmentGW
+            // 
+            treatmentGW.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            treatmentGW.Location = new Point(53, 29);
+            treatmentGW.Name = "treatmentGW";
+            treatmentGW.Size = new Size(1042, 495);
+            treatmentGW.TabIndex = 0;
+            // 
+            // ShowTreatmentDetails
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1223, 590);
+            Controls.Add(treatmentGW);
+            Name = "ShowTreatmentDetails";
+            Text = "ShowTreatmentDetails";
+            Load += ShowTreatmentDetails_Load;
+            ((System.ComponentModel.ISupportInitialize)treatmentGW).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView treatmentGW;
+    }
+}
---
+++ Medicare Medical Center/ShowTreatmentDetails.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Medicare_Medical_Center
+{
+    public partial class ShowTreatmentDetails : Form
+    {
+        SqlConnection con;
+        SqlCommand cm;
+        SqlDataAdapter da;
+        DataTable dt;
+        string connectionString = "Data Source=Master;Initial Catalog=Medicare-Medical-Center-DB;Integrated Security=True;TrustServerCertificate=True";
+
+        public ShowTreatmentDetails()
+        {
+            InitializeComponent();
+            con = new SqlConnection(connectionString);
+        }
+
+        private void RefreshDataGridView()
+        {
+            try
+            {
+                con.Open();
+                string query = "SELECT * FROM Treatment";
+                da = new SqlDataAdapter(query, con);
+                dt = new DataTable();
+                da.Fill(dt);
+                treatmentGW.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        private void ShowTreatmentDetails_Load(object sender, EventArgs e)
+        {
+            RefreshDataGridView();
+        }
+    }
+}
