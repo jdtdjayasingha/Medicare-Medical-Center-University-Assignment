b60ef728e50dfd17029b394918e8dd06c621e206
Tharindu Dilshan <jdtdjayasingha@gmail.com>
2024-04-24 19:27:14 +0530

    Created the Show Appointment Details Screen & Show Appointment data in Grid View

---
+++ Medicare Medical Center/ShowAppointmentDetails.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Medicare_Medical_Center
+{
+    partial class ShowAppointmentDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            appointmentGW = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)appointmentGW).BeginInit();
+            SuspendLayout();
+            // 
+            // appointmentGW
+            // 
+            appointmentGW.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            appointmentGW.Location = new Point(37, 29);
+            appointmentGW.Name = "appointmentGW";
+            appointmentGW.Size = new Size(1113, 546);
+            appointmentGW.TabIndex = 0;
+            // 
+            // ShowAppointmentDetails
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1174, 620);
+            Controls.Add(appointmentGW);
+            Name = "ShowAppointmentDetails";
+            Text = "ShowAppointmentDetails";
+            Load += ShowAppointmentDetails_Load;
+            ((System.ComponentModel.ISupportInitialize)appointmentGW).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView appointmentGW;
+    }
+}
---
+++ Medicare Medical Center/ShowAppointmentDetails.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Medicare_Medical_Center
+{
+    public partial class ShowAppointmentDetails : Form
+    {
+        SqlConnection con;
+        SqlCommand cm;
+        SqlDataAdapter da;
+        DataTable dt;
+        string connectionString = "Data Source=Master;Initial Catalog=Medicare-Medical-Center-DB;Integrated Security=True;TrustServerCertificate=True";
+
+        public ShowAppointmentDetails()
+        {
+            InitializeComponent();
+            con = new SqlConnection(connectionString);
+        }
+
+        private void RefreshDataGridView()
+        {
+            try
+            {
+                con.Open();
+                string query = "SELECT * FROM Appointment";
+                da = new SqlDataAdapter(query, con);
+                dt = new DataTable();
+                da.Fill(dt);
+                appointmentGW.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void ShowAppointmentDetails_Load(object sender, EventArgs e)
+        {
+            RefreshDataGridView();
+        }
+    }
+}
